be0e49643021dd6fb44676b8c42f8f49d2195fcf
Douglas Jesus <dougecomp@gmail.com>
2023-12-05 21:42:05 -0300

    adicionando código ccu

---
+++ play-lcd/ccu.s
@@ -0,0 +1,48 @@
+.EQU UART3_RST, 0x02D8 @BUS_SOFT_RST_REG4
+.EQU UART3_GATING, 0x006C @BUS_CLK_GATING_REG3
+.EQU APB2_CLK_SRC_SEL, 0x0058 @APB2_CFG_REG
+
+.macro MapeamentoMemoria
+    @sys_open
+    LDR R0, =fileName @ R0 = nome do arquivo
+    MOV R1, #2 @ O_RDWR (permissao de leitura e escrita pra arquivo)
+    MOV R7, #5 @ sys_open
+    SVC 0
+    MOV R4, R0 @ salva o descritor do arquivo.
+
+    @sys_mmap2
+    MOV R0, #0 @ NULL (SO escolhe o endereco)
+    LDR R1, =pagelen
+    LDR R1, [R1] @ tamanho da pagina de memoria
+    MOV R2, #3 @ protecao leitura ou escrita
+    MOV R3, #1 @ memoria compartilhada
+    LDR R5, =gpioaddr @ endereco GPIO / 4096
+    LDR R5, [R5]
+    MOV R7, #192 @sys_mmap2
+    SVC 0
+    MOV R8, R0
+.endm
+
+.macro configuracaoccu
+    @desativo o enable colocando o bit 19 do endereco em 1
+    LDR R0, [R8, #UART3_RST]
+    MOV R5, #1
+    LSL R5, R5, #19
+    ORR R0, R0, R5
+    STR R0, [R8, #UART3_RST]
+
+    @habilitar clock PLL_PERIPH0
+    LDR R0, [R8, #APB2_CLK_SRC_SEL]
+    MOV R5, #1
+    LSL R5, R5, #25
+    ORR R0, R0, R5
+    STR R0, [R8, #APB2_CLK_SRC_SEL]
+
+    @habilito clock na uart3
+    LDR R0, [R8, #UART3_GATING]
+    MOV R5, #1
+    LSL R5, R5, #19
+    ORR R0, R0, R5
+    STR R0, [R8, #UART3_GATING]
+
+.endm
