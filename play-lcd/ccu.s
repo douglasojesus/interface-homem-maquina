3b9b7d3ef7afc00a6f2dcc0b7b2ab7c4e4764cea
douglasojesus <dougecomp@gmail.com>
2023-12-06 16:59:49 -0300

    configuração ccu

--- play-lcd/ccu.s
+++ play-lcd/ccu.s
@@ -1,8 +1,9 @@
 .EQU UART3_RST, 0x02D8 @BUS_SOFT_RST_REG4
 .EQU UART3_GATING, 0x006C @BUS_CLK_GATING_REG3
 .EQU APB2_CLK_SRC_SEL, 0x0058 @APB2_CFG_REG
+.EQU PLL_ENABLE, 0x0028 @PLL_PERIPH0_CTRL_REG
 
-.macro MapeamentoMemoria
+.macro  mapeamentomemoriaccu
     @sys_open
     LDR R0, =fileName @ R0 = nome do arquivo
     MOV R1, #2 @ O_RDWR (permissao de leitura e escrita pra arquivo)
@@ -37,6 +38,27 @@
     LSL R5, R5, #25
     ORR R0, R0, R5
     STR R0, [R8, #APB2_CLK_SRC_SEL]
+   
+    @habilito pll_enable
+    LDR R0, [R8, #PLL_ENABLE]
+    MOV R5, #1
+    LSL R5, R5, #31
+    ORR R0, R0, R5
+    STR R0, [R8, #PLL_ENABLE] 
+    
+    @habilito PLL_CLK_OUT_EN
+    LDR R0, [R8, #PLL_ENABLE]
+    MOV R5, #1
+    LSL R5, R5, #24
+    ORR R0, R0, R5
+    STR R0, [R8, #PLL_ENABLE]
+
+    @desativo PLL_24M_POST_DIV
+    LDR R0, [R8, #PLL_ENABLE]
+    MOV R5, #1
+    LSL R5, R5, #18
+    EOR R0, R0, R5
+    STR R0, [R8, #PLL_ENABLE]
 
     @habilito clock na uart3
     LDR R0, [R8, #UART3_GATING]
@@ -46,3 +68,8 @@
     STR R0, [R8, #UART3_GATING]
 
 .endm
+
+.data
+    .data
+    uartaddr: .word 0x01C20 @endereço base da uart0
+    pagelen: .word 0x1000
