dc681c396fa05f24dcb8b3327c593b0a1b39d5a5
Douglas Jesus <dougecomp@gmail.com>
2023-12-08 00:43:01 -0300

    implementando chamada da UART

--- play-lcd/main_uart.s
+++ play-lcd/main_uart.s
@@ -1,17 +1,38 @@
 .include "uart.s"
+.include "ccu.s"
+.include "gpio.s"
 
 .global _start
 
 _start:
-        UART_Mapeamento
-        UART_Config
-        UART_tx_byte  
-             
-_end:   mov     R0, #0      
-        mov     R7, #1      
-        svc     0  
+    MapeamentoMemoria
+    mapeamentomemoriaccu
+    configuracaoccu
+    mapeamento_uart
+    set_pin_uart UART_TX
+    configuracaouart 
 
-.data
+    UART_TX #0b101010
+            
+end:   
+    mov     R0, #0      
+    mov     R7, #1      
+    svc     0  
 
-devMen:   .asciz  "/dev/mem"
+.data  
+    fileName: .asciz  "/dev/mem"
+    uartaddr: .word 0x01C28 @endereço base da uart0
+    gpioaddr: .word 0x01C20 @endereço base da uart0
+    pagelen: .word 0x1000
 
+    UART_TX:
+        .word 0x4
+        .word 0x18
+        .word 0xe
+        .word 0x10 
+
+    UART_RX:
+        .word 0x4
+        .word 0x14
+        .word 0xd
+        .word 0x10
