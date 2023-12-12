33c0bb757b8cbcab06f6742c73a086d81b455469
douglasojesus <dougecomp@gmail.com>
2023-12-12 13:11:13 -0300

    fix: inicializando r9

--- play-lcd/split.s
+++ play-lcd/split.s
@@ -1,7 +1,8 @@
 .macro catchDigits
+    mov r9, #0
     mov r10, #10 @r10=10
-    sdiv r9, r12, r10 @r9= primeiro digito 
+    sdiv r9, r6, r10 @r9= primeiro digito 
 
     mul r10, r9, r10
-    sub r12, r12, r10 @r6= segundo digito
+    sub r6, r6, r10 @r6= segundo digito
 .endm
